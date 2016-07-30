6b2a1412b8ddde145faf6168ca379e7602e57a17
Eyal Perry <e.perry.pa@gmail.com>
2016-07-30 10:07:03 +0300

    10.1

--- lab 10.1/Personnel/Personnel/Program.cs
+++ lab 10.1/Personnel/Personnel/Program.cs
@@ -9,6 +9,16 @@ namespace Personnel
 {
     class Program
     {
+       /**
+       * You are not handling exceptions at all, which is expected at this point in the course.
+       * Consider this:
+       * https://msdn.microsoft.com/en-us/library/ms164917.aspx
+       */
+
+        /**Bug: you are not disposing of the string reader
+        * Consider this:
+        * https://msdn.microsoft.com/en-us/library/3bwa4xa9(v=vs.110).aspx
+        */
         static void Main(string[] args)
         {
             ReadAndPrintFile(Directory.GetCurrentDirectory() + @"\..\..\names.txt");
@@ -27,7 +37,7 @@ namespace Personnel
 
         private static void PrintList(List<string> list)
         {
-            foreach(var line in list)
+            foreach (var line in list)
             {
                 Console.WriteLine(line);
             }
